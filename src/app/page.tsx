448c97ed2ff4d963ef5880411cb2b33382495590
Chris Kemeza <ckemeza1@gmail.com>
2025-09-19 17:03:11 -0400

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <>
     <div className="sit3-grid">
       <div className="py-4 pl-4 min-w-[150px]">
-        <Image src='/wwwashed-logo.png' alt="logo for wwwashed" width="300" height="420" className="max-w-[250px] h-auto w-full" />
+        <Image src='/_wwwashed-logo.png' alt="logo for wwwashed" width="300" height="420" className="max-w-[250px] h-auto w-full" />
       </div>
 
       <main className="site-main py-4 px-4">
